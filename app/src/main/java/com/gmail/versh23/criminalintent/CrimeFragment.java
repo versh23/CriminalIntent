fd0f99edcabafee7be42a0b7e2bd91a3fa234ffa
versh23 <versh23@gmail.com>
2017-08-21 00:11:59 +0300

    Initial commit

---
+++ app/src/main/java/com/gmail/versh23/criminalintent/CrimeFragment.java
@@ -0,0 +1,28 @@
+package com.gmail.versh23.criminalintent;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class CrimeFragment extends Fragment {
+
+    private Crime mCrime;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mCrime = new Crime();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_crime, container, false);
+    }
+
+}
