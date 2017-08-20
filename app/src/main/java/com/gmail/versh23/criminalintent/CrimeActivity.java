97e1e08c96fe119b4bc4b99ff37484cf852f8d76
versh23 <versh23@gmail.com>
2017-08-21 00:22:57 +0300

    update

--- app/src/main/java/com/gmail/versh23/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/gmail/versh23/criminalintent/CrimeActivity.java
@@ -1,5 +1,7 @@
 package com.gmail.versh23.criminalintent;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class CrimeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        Fragment fr = fm.findFragmentById(R.id.fragment_container);
+
+        if(fr == null) {
+            fr = new CrimeFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fr)
+                    .commit();
+        }
     }
 }
